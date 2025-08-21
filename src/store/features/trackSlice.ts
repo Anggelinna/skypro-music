9dc54aefedf6c5cdacaad4b87261a8d1f8cb7880
Anggelinna <lina.koloytanova.03@yandex.ru>
2025-08-21 22:46:53 +0500

    ll

--- src/store/features/trackSlice.ts
+++ src/store/features/trackSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { TrackType } from "../../sharedTypes/sharedTypes";
+import { createSlice, current, PayloadAction } from "@reduxjs/toolkit";
+import { TrackType } from "@/sharedTypes/sharedTypes";
 
 type TrackState = {
   tracks: TrackType[];
