b6c8c223028113e4441b3ac7e696376abd5709b1
Anggelinna <lina.koloytanova.03@yandex.ru>
2025-09-14 20:04:51 +0500

    .

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -28,14 +28,11 @@ export const getUniqueValuesByKey = (
   arr: TrackType[],
   key: keyof TrackType
 ): string[] => {
-  // Используем Set для хранения уникальных значений
   const uniqueValues = new Set<string>();
 
-  // Проходим по каждому объекту в массиве
   arr.forEach((item) => {
     const value = item[key];
 
-    // Если значение — массив строк
     if (Array.isArray(value)) {
       value.forEach((v) => {
         if (v) {
@@ -43,13 +40,11 @@ export const getUniqueValuesByKey = (
         }
       });
     }
-    // Если значение — строка
     else if (typeof value === "string") {
       uniqueValues.add(value);
     }
   });
 
-  // Преобразуем Set обратно в массив и возвращаем
   return Array.from(uniqueValues);
 };
 
